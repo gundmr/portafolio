5a67e8c9f68d969ec3686b8d205d71e7f0d9a778
gundrm <rachelm.gundy@gmail.com>
2019-09-22 14:25:32 -0700

    hero styling

--- src/App.js
+++ src/App.js
@@ -7,6 +7,9 @@ import './App.css';
 
 // INTERNAL COMPONENTS
 import Footer from './components/Footer';
+import HomePage from './pages/HomePage';
+import AboutPage from './pages/AboutPage';
+import ContactPage from './pages/ContactPage';
 
 class App extends React.Component {
 
@@ -52,11 +55,17 @@ class App extends React.Component {
                 <Link className="nav-link" to="/about">About</Link>
                 <Link className="nav-link" to="/contact">Contact</Link>
               </Nav>
-
             </Navbar.Collapse>
           </Navbar>
 
+{/* ROUTES - IMPORTED */}
+          <Route path="/" exact render={() => <HomePage title={this.state.home.title} subTitle={this.state.home.subTitle} text={this.state.home.text}/>} />
 
+          <Route path="/about" render={() => <AboutPage title={this.state.about.title}/>} />
+
+          <Route path="/contact" render={() => <ContactPage title={this.state.contact.title}/>} />
+
+{/* FOOTER - IMPORTED */}
           <Footer />
 
         </Container>
