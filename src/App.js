e0e4a85fb92265b33d27a56e62dddc5ef7df0e2c
gundrm <rachelm.gundy@gmail.com>
2019-09-22 13:02:31 -0700

    navbar

--- src/App.js
+++ src/App.js
@@ -1,4 +1,8 @@
 import React from 'react';
+import {BrowserRouter as Router, Route, Link } from 'react-router-dom';
+import Container from 'react-bootstrap/Container';
+import Navbar from 'react-bootstrap/Navbar'; 
+import Nav from 'react-bootstrap/Nav'; 
 import './App.css';
 
 class App extends React.Component {
@@ -26,12 +30,31 @@ class App extends React.Component {
       contact: {
       title: 'Let\'s Talk'
       }
+    }
   }
 
-  render(){
+  render() {
     return (
-      <div>Hello</div>
+      <Router>
+        <Container className="p-0" fluid={true}>
 
+{/* NAV BAR DETAILS */}
+          <Navbar className="border-bottom" bg="transparent" expand="lg">
+            <Navbar.Brand>Rachel Gundy</Navbar.Brand>
+
+            <Navbar.Toggle className="border-0" aria-controls="navbar-toggle"/>
+            <Navbar.Collapse id="navbar-toggle">
+              <Nav className="ml-auto">
+                <Link className="nav-link" to="/">Home</Link>
+                <Link className="nav-link" to="/about">About</Link>
+                <Link className="nav-link" to="/contact">Contact</Link>
+              </Nav>
+
+            </Navbar.Collapse>
+          </Navbar>
+
+        </Container>
+      </Router>
     );
   }
 }
