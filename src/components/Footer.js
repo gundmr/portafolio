6533bdd780f5f3abdd73a77e69c861516773a6ae
gundrm <rachelm.gundy@gmail.com>
2019-09-22 17:30:03 -0700

    aboutme data and responsive

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,21 +1,15 @@
 import React from 'react';
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
-import Col from 'react-bootstrap/Col';
+
 
 function Footer() {
 
     return(
         <footer className="mt-5">
             <Container fluid={true}>
-                <Row className="border-top justify-content-between p-3">
-                    <Col className="p-0" md={3} sm={12}>
-                        Rachel Gundy
-                    </Col>
-                    <Col className="p-0 d-flex justify-content-end" md={3}>
-                    This site was made by Rachel Gundy
-                    </Col>
-
+                <Row className="border-top justify-content-center p-3">
+                    <p>Copyright &copy;</p>
                 </Row>
             </Container>
         </footer>
